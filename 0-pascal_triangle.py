997cc1ec4c9ee65975c2687a983d8932999accf9
Mrelshimy <mraafat.elsayed@gmail.com>
2024-05-31 02:12:36 +0300

    task 0

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    """Pascal traingle implementation function"""
+    triangle = []
+    if n <= 0:
+        return triangle
+    else:
+        for i in range(n):
+            row = [1]
+            for j in range(i):
+                row.append(1)
+            triangle.append(row)
+        for row in range(2, n):
+            for item in range(1, row):
+                triangle[row][item] = triangle[row-1][item-1]\
+                      + triangle[row-1][item]
+    return (triangle)
