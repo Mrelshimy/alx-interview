ce4c483f7a262b626c09cc3bef0035e07383bb29
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-05 01:31:15 +0300

    reduce syntax

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,7 @@ def canUnlockAll(boxes):
     """function to solve lockboxes issue"""
     openBoxes = [0]
     for key in boxes[0]:
-        openBoxes.append(key if key < len(boxes) or key not in openBoxes else '')
+        openBoxes.append(key if key < len(boxes) or key not in openBoxes else None)
     for key in openBoxes:
         for newKey in boxes[key]:
             if newKey in openBoxes or newKey >= len(boxes):
