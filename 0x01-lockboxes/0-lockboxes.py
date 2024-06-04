b5ac079c407d2477b7c3e58c030027c67d538e84
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-05 01:32:55 +0300

    bugfix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,7 +6,10 @@ def canUnlockAll(boxes):
     """function to solve lockboxes issue"""
     openBoxes = [0]
     for key in boxes[0]:
-        openBoxes.append(key if key < len(boxes) or key not in openBoxes else None)
+        if key >= len(boxes) or key in openBoxes:
+            continue
+        else:
+            openBoxes.append(key)
     for key in openBoxes:
         for newKey in boxes[key]:
             if newKey in openBoxes or newKey >= len(boxes):
