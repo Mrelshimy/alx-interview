d49400373f40de34d2bfd473462071df7f5fc99f
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-13 17:29:13 +0300

    bug fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""  Minimum operations module """
+""" Minimum operations Module"""
 
 
-def minOperations(n: int) -> int:
-  """ Min operations function """
+def minoperations(n: int) -> int:
+    """ Function for Min operations """
 
-  if n <= 0 or not isinstance(n, int):
-    return 0
-  ops = 2
-  printed = [1, 2]
-  copied = [1]
-  while printed[-1] <= n:
-    if (n - printed[-1]) % printed[-1]:
-      ops += 2
-      printed.append(printed[-1] * 2)
-    else:
-      ops += 1
-      printed.append(printed[-1] + copied[-1])
-    copied.append(printed[-1] - printed[-2])
-  return ops
+    if n <= 1 or not isinstance(n, int):
+        return 0
+    ops = 2
+    printed = [1, 2]
+    copied = [1]
+    while printed[-1] <= n:
+        if (n - printed[-1]) % printed[-1] == 0:
+            ops += 2
+            printed.append(printed[-1] * 2)
+        else:
+            ops += 1
+            printed.append(printed[-1] + copied[-1])
+        copied.append(printed[-1] - printed[-2])
+    return ops
