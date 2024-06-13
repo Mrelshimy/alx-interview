44eeca928f20d02d8d4a73fff362f6849be60aa4
Mrelshimy <mraafat.elsayed@gmail.com>
2024-06-13 18:34:48 +0300

    bug fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,13 +9,13 @@ def minOperations(n: int) -> int:
         return 0
     ops = 2
     printed = [1, 2]
-    copied = [1]
+    copied = 1
     while printed[-1] < n:
         if (n - printed[-1]) % printed[-1] == 0:
             ops += 2
             printed.append(printed[-1] * 2)
         else:
             ops += 1
-            printed.append(printed[-1] + copied[-1])
-        copied.append(printed[-1] - printed[-2])
+            printed.append(printed[-1] + copied)
+        copied = (printed[-1] - printed[-2])
     return ops
