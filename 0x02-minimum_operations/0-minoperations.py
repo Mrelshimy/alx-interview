6d2d2f06f0a9d5c28a83991725f67e8899eeaef8
Mohamed Raafat <138755670+Mrelshimy@users.noreply.github.com>
2024-06-13 15:11:40 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,17 @@
 def minOperations(n: int) -> int:
   """ Min operations function """
 
-    if n <= 0 or not isinstance(n, int):
-        return 0
-    ops = 2
-    printed = [1, 2]
-    copied = [1]
-    while printed[-1] < n:
-        if (n - printed[-1]) % printed[-1]:
-            ops += 2
-            printed.append(printed[-1] * 2)
-        else:
-            ops += 1
-            printed.append(printed[-1] + copied[-1])
-        copied.append(printed[-1] - printed[-2])
-    return ops
+  if n <= 0 or not isinstance(n, int):
+    return 0
+  ops = 2
+  printed = [1, 2]
+  copied = [1]
+  while printed[-1] <= n:
+    if (n - printed[-1]) % printed[-1]:
+      ops += 2
+      printed.append(printed[-1] * 2)
+    else:
+      ops += 1
+      printed.append(printed[-1] + copied[-1])
+    copied.append(printed[-1] - printed[-2])
+  return ops
